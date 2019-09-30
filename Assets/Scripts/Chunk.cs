60446d38b9bca8e8a102b93ce7be4ebf35fc27f2
rafaeltab <ikbenrafaelbieze2001@gmail.com>
2019-09-30 23:20:12 +0200

    Implemented Marching Cubes

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,4 +1,5 @@
-﻿using NoiseTest;
+﻿using Assets.Scripts.Visualisers;
+using NoiseTest;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,11 +14,12 @@ namespace Assets.Scripts
         public readonly Vector3Int position;
         public readonly float trength;
         private Mesh mesh = new Mesh();
-        private readonly int size;
+        private int size;
         private readonly float noiseScale;
         private readonly long seed;
         private readonly float threshold;
         private float[,,] vals;
+        private IMeshVisualiser mv;
 
         private bool generated = false;
 
@@ -29,6 +31,8 @@ namespace Assets.Scripts
         /// <param name="size">The size of the chunk</param>
         public Chunk(Vector3Int pos,float trength,long seed,float threshold, int size = 16, float noiseScale = 1f)
         {
+            mv = new MarchingVisualiser(threshold);
+            this.seed = seed+mv.sizeOffset();
             position = pos;
             this.trength = trength;
             this.size = size;
@@ -37,7 +41,7 @@ namespace Assets.Scripts
                 throw new ArgumentException("noiseScale is out of bounds, it is less then or equal to 0");
             }
             this.noiseScale = noiseScale;
-            this.seed = seed;
+            
             this.threshold = threshold;
         }
 
@@ -54,10 +58,8 @@ namespace Assets.Scripts
         /// Pre-Generate the chunk
         /// </summary>
         public void Generate()
-        {
-            vals = Gen();
-            CreateMesh(vals);                      
-
+        { 
+            mesh = mv.Visualize(Gen(size),mesh,size);
             generated = true;
         }
 
@@ -83,22 +85,22 @@ namespace Assets.Scripts
             filter.mesh = mesh;
             go.SetActive(true);
             go.name = $"Chunk {position.x}, {position.y}, {position.z}";
-            go.transform.position = position*size;
+            go.transform.position = position*(size);
         }
 
         /// <summary>
         /// Create the values array
         /// </summary>
         /// <returns>Float values</returns>
-        private float[,,] Gen()
+        private float[,,] Gen(int size)
         {
             SimplexNoise sn = new SimplexNoise(seed);
-            float[,,] values = new float[size, size, size];
-            for (int x = 0; x < size; x++)
+            float[,,] values = new float[size + mv.sizeOffset(), size + mv.sizeOffset(), size + mv.sizeOffset()];
+            for (int x = 0; x < size+mv.sizeOffset(); x++)
             {
-                for (int y = 0; y < size; y++)
+                for (int y = 0; y < size + mv.sizeOffset(); y++)
                 {
-                    for (int z = 0; z < size; z++)
+                    for (int z = 0; z < size + mv.sizeOffset(); z++)
                     {
                         float sampleX = (x + position.x * size) * noiseScale;
                         float sampleY = (y + position.y * size) * noiseScale;
@@ -111,116 +113,6 @@ namespace Assets.Scripts
             }
 
             return values;
-        }
-
-        /// <summary>
-        /// Create a mesh from a float array
-        /// </summary>
-        /// <param name="values">float array</param>
-        /// <returns>generated mesh</returns>
-        private void CreateMesh(float[,,] values)
-        {
-            List<Vector3> vertices = new List<Vector3>();
-            List<int> triangles = new List<int>();
-            List<Vector2> uvs = new List<Vector2>();
-
-            for (int x = 0; x < size; x++)
-            {
-                for (int y = 0; y < size; y++)
-                {
-                    for (int z = 0; z < size; z++)
-                    {
-                        if (Enabled(values[x, y, z]))
-                        {
-                            DoFace(x, y, z, values, vertices, triangles, uvs);
-                        }
-                    }
-                }
-            }
-
-            mesh.Clear();
-            mesh.vertices = vertices.ToArray();
-            mesh.triangles = triangles.ToArray();
-            mesh.RecalculateNormals();
-        }
-
-        /// <summary>
-        /// Generate a single face
-        /// </summary>
-        private void DoFace(int x, int y, int z, float[,,] values, List<Vector3> vertices, List<int> triangles, List<Vector2> uvs)
-        {
-            Vector3 current = new Vector3(x, y, z);
-            #region x
-            if (values.GetLength(0) == x + 1)
-            {
-                GeneratorHelper.CreateFace(GeneratorHelper.drawRight(), vertices, triangles, uvs, current);
-            }
-            else if (!Enabled(values[x + 1, y, z]))
-            {
-                GeneratorHelper.CreateFace(GeneratorHelper.drawRight(), vertices, triangles, uvs, current);
-            }
-
-            if (x == 0)
-            {
-                GeneratorHelper.CreateFace(GeneratorHelper.drawLeft(), vertices, triangles, uvs, current);
-            }
-            else if (!Enabled(values[x - 1, y, z]))
-            {
-                GeneratorHelper.CreateFace(GeneratorHelper.drawLeft(), vertices, triangles, uvs, current);
-            }
-            #endregion x
-
-            #region y
-            if (values.GetLength(1) == y + 1)
-            {
-
-                GeneratorHelper.CreateFace(GeneratorHelper.drawUp(), vertices, triangles, uvs, current);
-            }
-            else if (!Enabled(values[x, y + 1, z]))
-            {
-
-                GeneratorHelper.CreateFace(GeneratorHelper.drawUp(), vertices, triangles, uvs, current);
-            }
-
-            if (y == 0)
-            {
-
-                GeneratorHelper.CreateFace(GeneratorHelper.drawDown(), vertices, triangles, uvs, current);
-            }
-            else if (!Enabled(values[x, y - 1, z]))
-            {
-
-                GeneratorHelper.CreateFace(GeneratorHelper.drawDown(), vertices, triangles, uvs, current);
-            }
-            #endregion y
-
-            #region z
-            if (values.GetLength(2) == z + 1)
-            {
-                GeneratorHelper.CreateFace(GeneratorHelper.drawFar(), vertices, triangles, uvs, current);
-            }
-            else if (!Enabled(values[x, y, z + 1]))
-            {
-                GeneratorHelper.CreateFace(GeneratorHelper.drawFar(), vertices, triangles, uvs, current);
-            }
-
-            if (z == 0)
-            {
-                GeneratorHelper.CreateFace(GeneratorHelper.drawNear(), vertices, triangles, uvs, current);
-            }
-            else if (!Enabled(values[x, y, z - 1]))
-            {
-                GeneratorHelper.CreateFace(GeneratorHelper.drawNear(), vertices, triangles, uvs, current);
-            }
-            #endregion z
-        }
-
-        /// <summary>
-        /// Check if a value is enabled
-        /// </summary>
-        public bool Enabled(float val)
-        {
-            return val >= threshold;
         }
     }
 }
---
+++ Assets/Scripts/Visualisers/VoxelVisualisor.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class VoxelVisualisor : IMeshVisualiser
+    {
+        private float threshold = 0.5f;
+
+        /// <summary>
+        /// Constructor, set threshold
+        /// </summary>
+        /// <param name="thres"></param>
+        public VoxelVisualisor(float thres)
+        {
+            threshold = thres;
+        }
+
+        public int sizeOffset()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Create a mesh from a float array
+        /// </summary>
+        /// <param name="values">float array</param>
+        /// <returns>generated mesh</returns>
+        public Mesh Visualize(float[,,] values, Mesh template, int size)
+        {
+            List<Vector3> vertices = new List<Vector3>();
+            List<int> triangles = new List<int>();
+            List<Vector2> uvs = new List<Vector2>();
+
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    for (int z = 0; z < size; z++)
+                    {
+                        if (Enabled(values[x, y, z]))
+                        {
+                            DoFace(x, y, z, values, vertices, triangles, uvs);
+                        }
+                    }
+                }
+            }
+
+            template.Clear();
+            template.vertices = vertices.ToArray();
+            template.triangles = triangles.ToArray();
+            template.RecalculateNormals();
+
+            return template;
+        }
+
+        /// <summary>
+        /// Generate a single face
+        /// </summary>
+        private void DoFace(int x, int y, int z, float[,,] values, List<Vector3> vertices, List<int> triangles, List<Vector2> uvs)
+        {
+            Vector3 current = new Vector3(x, y, z);
+            #region x
+            if (values.GetLength(0) == x + 1)
+            {
+                GeneratorHelper.CreateFace(GeneratorHelper.drawRight(), vertices, triangles, uvs, current);
+            }
+            else if (!Enabled(values[x + 1, y, z]))
+            {
+                GeneratorHelper.CreateFace(GeneratorHelper.drawRight(), vertices, triangles, uvs, current);
+            }
+
+            if (x == 0)
+            {
+                GeneratorHelper.CreateFace(GeneratorHelper.drawLeft(), vertices, triangles, uvs, current);
+            }
+            else if (!Enabled(values[x - 1, y, z]))
+            {
+                GeneratorHelper.CreateFace(GeneratorHelper.drawLeft(), vertices, triangles, uvs, current);
+            }
+            #endregion x
+
+            #region y
+            if (values.GetLength(1) == y + 1)
+            {
+
+                GeneratorHelper.CreateFace(GeneratorHelper.drawUp(), vertices, triangles, uvs, current);
+            }
+            else if (!Enabled(values[x, y + 1, z]))
+            {
+
+                GeneratorHelper.CreateFace(GeneratorHelper.drawUp(), vertices, triangles, uvs, current);
+            }
+
+            if (y == 0)
+            {
+
+                GeneratorHelper.CreateFace(GeneratorHelper.drawDown(), vertices, triangles, uvs, current);
+            }
+            else if (!Enabled(values[x, y - 1, z]))
+            {
+
+                GeneratorHelper.CreateFace(GeneratorHelper.drawDown(), vertices, triangles, uvs, current);
+            }
+            #endregion y
+
+            #region z
+            if (values.GetLength(2) == z + 1)
+            {
+                GeneratorHelper.CreateFace(GeneratorHelper.drawFar(), vertices, triangles, uvs, current);
+            }
+            else if (!Enabled(values[x, y, z + 1]))
+            {
+                GeneratorHelper.CreateFace(GeneratorHelper.drawFar(), vertices, triangles, uvs, current);
+            }
+
+            if (z == 0)
+            {
+                GeneratorHelper.CreateFace(GeneratorHelper.drawNear(), vertices, triangles, uvs, current);
+            }
+            else if (!Enabled(values[x, y, z - 1]))
+            {
+                GeneratorHelper.CreateFace(GeneratorHelper.drawNear(), vertices, triangles, uvs, current);
+            }
+            #endregion z
+        }
+
+        /// <summary>
+        /// Check if a value is enabled
+        /// </summary>
+        private bool Enabled(float val)
+        {
+            return val >= threshold;
+        }
+    }
+}
