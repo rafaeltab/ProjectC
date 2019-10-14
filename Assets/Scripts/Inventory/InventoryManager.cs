32564fbb0ffb4db9f048596b7044c0f1536781cd
FranciscoNgo <0966462@hr.nl>
2019-10-14 12:07:23 +0200

    Revert "Revert "Merge branch 'inventory' into demo""
    This reverts commit 3b931895c794a6093db3d296f88960a502e3a827.

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,326 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+    private bool inventoryEnabled = false;
+    public GameObject inventoryPanel;
+    public GameObject hotbarPanel;
+    public GameObject itemSlotPrefab;
+    public int startSlotRows = 4;
+    private int slotRow;
+    public static List<ItemSlot> inventoryList = new List<ItemSlot>();
+    public GameObject canvas;
+    public static Vector3 offsetInventory;
+    public static Vector3 offsetHotbar;
+
+    public class ItemSlot
+    {
+        public GameObject slotObj { get; set; }
+        public int slotID { get; set; }
+        public GameObject itemObj { get; set; }
+        public ItemDatabase.Item item { get; set; }
+        public int amount { get; set; }
+        public GameObject hotbarObj { get; set; }
+
+        /// <summary>
+        /// Class for the item slots
+        /// </summary>
+        /// <param name="slotObj">GameObject of the item slot</param>
+        /// <param name="slotID">ID of the item slot</param>
+        /// <param name="item">The item in the item slot</param>
+        /// <param name="amount">How much of the item is in the slot</param>
+        /// <param name="hotbarObj">GameObject of the hotbar slot, is null if the item slot is not a hotbar slot (not the first 5 item slots at the bottom in the inventory)</param>
+        public ItemSlot(GameObject slotObj, int slotID, ItemDatabase.Item item, int amount, GameObject hotbarObj)
+        {
+            this.slotObj = slotObj;
+            this.slotObj.transform.GetComponent<Slot>().itemSlot = this;
+            this.slotID = slotID;
+            this.hotbarObj = hotbarObj;
+
+            this.itemObj = this.slotObj.transform.GetChild(0).gameObject;
+
+            initiateItem(item, amount);
+        }
+
+
+        /// <summary>
+        /// Refreshes the item in the item slot with a new item
+        /// </summary>
+        /// <param name="item">The new item in the item slot</param>
+        /// <param name="amount">How much of the item there is</param>
+        public void initiateItem(ItemDatabase.Item item, int amount)
+        {
+            this.item = item;
+            this.amount = amount;
+            
+            this.itemObj.GetComponent<Image>().sprite = this.item.sprite;
+            this.itemObj.name = this.item.title;
+
+
+            if (this.hotbarObj != null) //If it's not a hotbar item slot
+            {
+                this.hotbarObj.transform.GetChild(0).GetComponent<Image>().sprite = this.item.sprite;
+                this.hotbarObj.transform.GetChild(0).name = this.item.title;
+            }
+
+            updateAmount();
+        }
+
+        /// <summary>
+        /// Updates the amount/stack text in the item slot
+        /// </summary>
+        public void updateAmount()
+        {
+            GameObject amountObj = this.itemObj.transform.GetChild(0).gameObject;
+            string amountText;
+            if (this.amount == 0) { amountText = ""; }
+            else { amountText = this.amount.ToString(); }
+            amountObj.GetComponent<TextMeshProUGUI>().text = amountText;
+
+            if (this.hotbarObj != null)
+            {
+                amountObj = this.hotbarObj.transform.GetChild(0).GetChild(0).gameObject;
+                amountObj.GetComponent<TextMeshProUGUI>().text = amountText;
+            }
+        }
+
+        /// <summary>
+        /// <para>Add the amount of the grabbed item to itemSlot</para>
+        /// If the amount is higher than the stackLimit, the amount of the itemSlot will be the stackLimit and the amount of the grabbed item will get the rest
+        /// </summary>
+        /// <param name="amount">The item amount of the grabbedItem</param>
+        public void sumItems(int amount)
+        {
+            this.amount += amount;
+
+            int rest = 0;
+            if (this.amount > Slot.grabbedItem.stackLimit) //If more than stacklimit
+            {
+                rest = this.amount - Slot.grabbedItem.stackLimit;
+                this.amount = Slot.grabbedItem.stackLimit;
+            }
+
+            initiateItem(Slot.grabbedItem, this.amount);
+            Slot.updateGrabbedItemObj(Slot.grabbedItem, rest);
+        }
+
+        /// <summary>
+        /// Adds 1 to the amount of itemSlot and remove 1 to the amount of the grabbed item
+        /// </summary>
+        public void addOne()
+        {
+            if (this.amount != Slot.grabbedItem.stackLimit)
+            {
+                this.amount += 1;
+                initiateItem(Slot.grabbedItem, this.amount);
+                Slot.updateGrabbedItemObj(Slot.grabbedItem, Slot.grabbedAmount - 1);
+            }
+        }
+
+
+        /// <summary>
+        /// Switch the items and amount in itemSlot and grabbed item
+        /// </summary>
+        /// <param name="item">The grabbedItem</param>
+        /// <param name="amount">The amount of the grabbed item</param>
+        public void switchItems(ItemDatabase.Item item, int amount)
+        {
+            ItemDatabase.Item itemTemp = this.item;
+            int amountTemp = this.amount;
+
+            initiateItem(item, amount);
+
+            Slot.updateGrabbedItemObj(itemTemp, amountTemp);
+        }
+
+        /// <summary>
+        /// Refresh the item slot with the item "Empty"
+        /// </summary>
+        public void emptyItemSlot()
+        {
+            initiateItem(ItemDatabase.fetchItemByID(0), 0);
+        }
+
+    }
+
+    /// <summary>
+    /// Fills the database, updates the UI size, adds the item slots to the inventory and keep them in a list and receive the hotbar slots
+    /// </summary>
+    private void Start()
+    {
+
+        ItemDatabase.fillDatabase();
+
+        updateUISize();
+
+        while (slotRow < startSlotRows)
+        {
+            addRow();
+        }
+
+        Equip.getHotbarItemSlots(inventoryList);
+
+    }
+
+
+    /* Item image square width and height = 50
+     * distance between item slots = 60
+     * distance between slot panel and inventory panel width = 20 (left and right)
+     * distance between slot panel and inventory panel height = 15 (up and down)
+     */
+
+
+    /// <summary>
+    /// Updates the UI size (inventory and hotbar dimensions) if the screen dimensions change
+    /// </summary>
+    public void updateUISize()
+    {
+        Vector2 canvasSize = canvas.transform.GetComponent<RectTransform>().sizeDelta;
+
+        RectTransform rectTransformInventory = inventoryPanel.transform.GetComponent<RectTransform>();
+
+        /* width inventory = 330
+         * Calculate left and right:
+         * (canvas width - width inventory) / 2 = left and right
+         * 
+         * height 1 row inventory = 80
+         * Calculate top and bottom:
+         * (canvas height - height inventory) / 2 = top and bottom
+         */
+
+        int inventoryWidth = 330;
+        int inventoryHeight = 80;
+
+        int extraDistance = 0; //Extra distance for hotbar
+        if (startSlotRows > 1)
+        {
+            extraDistance = 5;
+        }
+
+        float inventoryBottomTop = (canvasSize.y - (60*(startSlotRows-1) + inventoryHeight + extraDistance)) / 2;
+        float inventoryLeftRight = (canvasSize.x - inventoryWidth) / 2;
+
+        // [ left - bottom ]
+        rectTransformInventory.offsetMin = new Vector2(inventoryLeftRight, inventoryBottomTop);
+        // [ right - top ]
+        rectTransformInventory.offsetMax = new Vector2(-inventoryLeftRight, -inventoryBottomTop);
+
+        offsetInventory = new Vector3(inventoryLeftRight + 20, inventoryBottomTop + 15, 0);
+
+
+        RectTransform rectTransformHotbar = hotbarPanel.transform.GetComponent<RectTransform>();
+
+        float hotbarHeight = canvasSize.y - inventoryHeight + 10;
+
+        // [ left - bottom ]
+        rectTransformHotbar.offsetMin = new Vector2(inventoryLeftRight + 10, hotbarHeight / 20);
+        // [ right - top ]
+        rectTransformHotbar.offsetMax = new Vector2(-inventoryLeftRight - 10, -hotbarHeight* 19/20);
+
+        offsetHotbar = new Vector3(inventoryLeftRight + 20, hotbarHeight / 20 + 10, 0);
+    }
+
+    /// <summary>
+    /// Adds a new row in the inventory
+    /// </summary>
+    public void addRow()
+    {
+
+        for (int i = 0; i < 5; i++)
+        {
+            int extraDistance = 0; //Extra distance for hotbar
+            if (slotRow > 0)
+            {
+                extraDistance = 5;
+            }
+
+            GameObject slotObj = Instantiate(itemSlotPrefab, new Vector3(265 + -60*i, 25 + 60*slotRow + extraDistance, 0) + offsetInventory, new Quaternion(0, 0, 0, 0), inventoryPanel.transform.GetChild(0));
+            slotObj.name = "Item Slot " + inventoryList.Count;
+
+            GameObject hotbarObj = null;
+            if (slotRow == 0)       //If hotbar (first 5 item slots)
+            {
+                hotbarObj = Instantiate(itemSlotPrefab, new Vector3(265 + -60*i, 25, 0) + offsetHotbar, new Quaternion(0, 0, 0, 0), hotbarPanel.transform.GetChild(0));
+                Destroy(hotbarObj.GetComponent<Slot>());
+                Destroy(hotbarObj.transform.Find("Hover").gameObject);
+                hotbarObj.name = "Hotbar Slot " + inventoryList.Count;
+            }
+
+            //ItemSlot itemSlot = new ItemSlot(slotObj, inventoryList.Count, ItemDatabase.fetchItemByID(0), 0, hotbarObj); //Default
+            ItemSlot itemSlot;
+            if (i % 2 == 0) { itemSlot = new ItemSlot(slotObj, inventoryList.Count, ItemDatabase.fetchItemByID(1), 32, hotbarObj); }
+            else { itemSlot = new ItemSlot(slotObj, inventoryList.Count, ItemDatabase.fetchItemByID(2), 50, hotbarObj); }
+
+            inventoryList.Add(itemSlot);
+        }
+
+        slotRow += 1;
+    }
+
+    /// <summary>
+    /// Returns an item Slot from inventoryList with the id
+    /// </summary>
+    /// <param name="id">The id of the item Slot</param>
+    /// <returns>The specified item Slot from the list</returns>
+    public static ItemSlot fetchItemSlotByID(int id)
+    {
+        return inventoryList[id];
+    }
+
+
+//WIP WIP WIP WIP WIP WIP WIP
+    /// <summary>
+    /// Gets called if the player picks up an item off the ground (WIP)
+    /// </summary>
+    /// <param name="item">The item the player picked up off the ground</param>
+    /// <param name="amount">How much of the item there is</param>
+    public static void pickUpItem(ItemDatabase.Item item, int amount)
+    {
+        //Do stuff
+        // if item already in inventory, add
+        // if item not in inventory or item in inventory but all full stacks, then put in new item slot (from last to first)
+        // if inventory full, don't pick up item
+
+        //This could be a method in a separate class (item entity class)
+    }
+//WIP WIP WIP WIP WIP WIP WIP
+
+
+    /// <summary>
+    /// Remove the item from the inventory and drop it on the ground (The item gets destroyed/removed for now)
+    /// </summary>
+    /// <param name="itemSlot">The item slot where the item is contained</param>
+    public static void dropItem(ItemSlot itemSlot)
+    {
+        //Remove item for now
+        itemSlot.emptyItemSlot();
+    }
+
+    // Update is called once per frame
+    /// <summary>
+    /// Open and close the inventory by pressing I
+    /// </summary>
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            inventoryEnabled = !inventoryEnabled;
+            inventoryPanel.SetActive(inventoryEnabled);
+            hotbarPanel.SetActive(!inventoryEnabled);
+        }
+
+    }
+
+    /// <summary>
+    /// Check for any screensize changes
+    /// </summary>
+    private void OnRectTransformDimensionsChange()
+    {
+        updateUISize();
+    }
+
+}
---
+++ Assets/Scripts/Inventory/Slot.cs
@@ -0,0 +1,203 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public InventoryManager.ItemSlot itemSlot;
+
+    public bool hovering = false;
+    public GameObject itemInfoBox;
+    public Vector3 offset;
+
+    public static GameObject grabbedItemObj;
+    public static ItemDatabase.Item grabbedItem;
+    public static int grabbedAmount;
+
+    public bool addOneDisabled = false;
+
+    /// <summary>
+    /// Get the itemInfoBox and grabbedItemObj GameObjects
+    /// </summary>
+    private void Start()
+    {
+        itemInfoBox = itemSlot.slotObj.transform.parent.parent.GetChild(2).gameObject;
+        grabbedItemObj = itemSlot.slotObj.transform.parent.parent.GetChild(1).gameObject;
+    }
+
+
+    /// <summary>
+    /// If the mouse hovers over the item slot, activate Hover and show item info
+    /// </summary>
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        itemSlot.slotObj.transform.Find("Hover").gameObject.SetActive(true);
+        hovering = true;
+        if (itemSlot.amount != 0)
+        {
+            itemInfoBox.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = itemSlot.item.title;
+            itemInfoBox.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>().text = itemSlot.item.description;
+            itemInfoBox.SetActive(true);
+        }
+    }
+
+
+    /// <summary>
+    /// <para>While hovering, move the item info to the mouse position</para>
+    /// <para>Moves the grabbed item to the mouse position if there is one</para>
+    /// <para>Picks up item if left mouse button is pressed above item</para>
+    /// <para>Picks up half of item if right mouse button is pressed above item</para>
+    /// <para>Press right mouse button while grabbing an item to move 1 amount of that item to an item slot</para>
+    /// <para>Drop an item by moving the mouse out of the inventory and pressing right mouse button</para>
+    /// </summary>
+    public void Update()
+    {
+        if (hovering)
+        {
+            offset = new Vector3(itemInfoBox.GetComponent<RectTransform>().rect.width / 2 + 10, itemInfoBox.GetComponent<RectTransform>().rect.height / -2 + 10, 0);
+            itemInfoBox.transform.position = Input.mousePosition + offset;
+
+            if (Input.GetMouseButtonDown(0)) //If left mouse button is down
+            {
+                if (grabbedAmount == 0 && itemSlot.item.id != 0) //If there is no grabbed item and the clicked item slot is not empty
+                {
+                    updateGrabbedItemObj(itemSlot.item, itemSlot.amount);
+
+                    itemSlot.emptyItemSlot();
+
+                    grabbedItemObj.SetActive(true);
+                    grabbedItemObj.GetComponent<CanvasGroup>().blocksRaycasts = false;
+                }
+                else
+                {
+                    if (itemSlot.item != grabbedItem && itemSlot.item.id != 0)
+                    {
+                        //Switch items
+                        itemSlot.switchItems(grabbedItem, grabbedAmount);
+                    }
+
+                    else if (grabbedAmount != 0)
+                    {
+                        //Sum items
+                        itemSlot.sumItems(grabbedAmount);
+                    }
+
+                }
+
+            }
+            else if (Input.GetMouseButtonDown(1)) //If right mouse button is down
+            {
+                if (grabbedAmount == 0 && itemSlot.item.id != 0) //If there is no grabbed item and the clicked item slot is not empty
+                {
+                    int amount;
+                    if (itemSlot.amount % 2 == 0)
+                    {
+                        amount = itemSlot.amount / 2;
+                    }
+                    else
+                    {
+                        amount = itemSlot.amount / 2 + 1;
+                    }
+
+                    updateGrabbedItemObj(itemSlot.item, amount);
+
+                    itemSlot.amount = itemSlot.amount - amount;
+                    itemSlot.updateAmount();
+
+                    addOneDisabled = true;
+
+                    grabbedItemObj.SetActive(true);
+                    grabbedItemObj.GetComponent<CanvasGroup>().blocksRaycasts = false;
+                }
+
+                //For adding 1 to itemSlot by clicking right mouse button
+                else if (grabbedAmount != 0 && itemSlot.item == grabbedItem || itemSlot.item.id == 0)
+                {
+                    addOneDisabled = true;
+                    itemSlot.addOne();
+                }
+
+            }
+
+            if (Input.GetMouseButton(1)) //While right mouse button is down
+            {
+                //For adding 1 to itemSlot by hovering with right mouse button
+                if (grabbedAmount != 0 && !addOneDisabled && itemSlot.item == grabbedItem || itemSlot.item.id == 0)
+                {
+                    addOneDisabled = true;
+                    itemSlot.addOne();
+                }
+
+            }
+
+
+            if (grabbedAmount == 0) //Check if there is a grabbed item
+            {
+                grabbedItemObj.SetActive(false);
+                grabbedItemObj.GetComponent<CanvasGroup>().blocksRaycasts = true;
+            }
+
+
+        }
+
+        // Move the grabbed item to the mouse position
+        if (grabbedItem != null)
+        {
+            grabbedItemObj.transform.position = Input.mousePosition;
+        }
+
+        //Check if mouse is outside inventory (For dropping items)
+        if (!CheckInventoryHover.hoverInventory)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                updateGrabbedItemObj(ItemDatabase.fetchItemByID(0), 0);
+                grabbedItemObj.SetActive(false);
+                grabbedItemObj.GetComponent<CanvasGroup>().blocksRaycasts = true;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Update the grabbedItemObj and values grabbedItem and grabbedAmount
+    /// </summary>
+    /// <param name="item">The grabbedItem</param>
+    /// <param name="amount">The amount of the grabbed item</param>
+    public static void updateGrabbedItemObj(ItemDatabase.Item item, int amount)
+    {
+        grabbedItem = item;
+        grabbedAmount = amount;
+        grabbedItemObj.transform.GetComponent<Image>().sprite = grabbedItem.sprite;
+        grabbedItemObj.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = grabbedAmount.ToString();
+    }
+
+
+    /// <summary>
+    /// If the mouse stops hovering, disable Hover and the item info
+    /// </summary>
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        hovering = false;
+        addOneDisabled = false;
+        itemSlot.slotObj.transform.Find("Hover").gameObject.SetActive(false);
+        itemInfoBox.SetActive(false);
+    }
+
+    /// <summary>
+    /// If the mouse stops hovering, disable Hover and the item info
+    /// </summary>
+    public void OnDisable()
+    {
+        hovering = false;
+        addOneDisabled = false;
+        if (itemSlot != null)
+        {
+            itemSlot.slotObj.transform.Find("Hover").gameObject.SetActive(false);
+            itemInfoBox.SetActive(false);
+        }
+        
+    }
+}
