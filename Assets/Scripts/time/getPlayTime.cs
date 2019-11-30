ca55bd030f9ec42e2ac0e1aa440cf250e1cfc02a
raygun333 <raygun333@outlook.com>
2019-12-01 00:59:32 +0100

    added timePlayed Script
    this has to be placed in every scene, it shoulddn't matter what object its on.
    it saves the time when application is closed btw

---
+++ Assets/Scripts/time/getPlayTime.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getPlayTime : MonoBehaviour
+{
+    float timeOld;
+    float timeRunning;
+    float rounded;
+    TimeSpan actualTime;
+    void Start()
+    {
+        //turns time into a readable format (just here to remember wehn it should be used)
+        //timeOld = PlayerPrefs.GetFloat("timeOld");
+        //print(timeOld);
+        //timeRunning = Time.time;
+        //rounded = UnityEngine.Mathf.Round(timeRunning);
+        //actualTime = TimeSpan.FromSeconds(rounded);
+    }
+
+    void OnApplicationQuit()
+    {
+        timeRunning = Time.time;
+        rounded = UnityEngine.Mathf.Round(timeRunning);
+        PlayerPrefs.SetFloat("timeOld", rounded+timeOld);
+    }
+}
