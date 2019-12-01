75a94557634d2c3357bac6cfaccd83073f3afa51
brtmn0 <37617892+brtmn0@users.noreply.github.com>
2019-12-01 21:16:33 +0100

    added hamster to maze level

---
+++ Assets/Scripts/MazeTest/hamstertime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class hamstertime : MonoBehaviour
+{
+    public GameObject hamster;
+    void Start()
+    {
+        Invoke("Hidehamster", 35.0f);
+    }
+
+    void Hidehamster()
+    {
+        hamster.SetActive(false);
+    }
+}
