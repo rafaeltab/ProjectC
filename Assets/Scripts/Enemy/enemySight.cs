d68230718afdcb4686b75cdfe231be196d52d3b3
raygun333 <raygun333@outlook.com>
2019-12-08 21:56:57 +0100

    Added Animations and Player detection
    animations are added into the project folder and the enemy can now detect the player

---
+++ Assets/Scripts/Enemy/enemySight.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySight : MonoBehaviour
+{
+    public float fieldOfViewAngle = 110f;
+    public bool detected;
+
+    public UnityEngine.AI.NavMeshAgent nav;
+    public SphereCollider col;
+    public Animator anim;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.transform.position - transform.position;
+        float angle = Vector3.Angle(direction, transform.forward);
+
+        
+        if(angle < fieldOfViewAngle * 0.5f)
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position + transform.up /2, direction.normalized, out hit, col.radius))
+            {
+                if(hit.collider.gameObject == player)
+                {
+                    detected = true;
+                    print(detected);
+                }else{
+                    detected = false;
+                }
+            }
+        }
+    }
+}
