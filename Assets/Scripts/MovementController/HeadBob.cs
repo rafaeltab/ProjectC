c9b9a44c971531f045d5ec88c8bc848451e2581e
raygun333 <raygun333@outlook.com>
2020-01-08 01:59:04 +0100

    fixed time manager and headbob
    dis man really touched the time scale but @rafaeltab doesnt want to admit it

--- Assets/Scripts/MovementController/HeadBob.cs
+++ Assets/Scripts/MovementController/HeadBob.cs
@@ -16,18 +16,5 @@ public class HeadBob : MonoBehaviour
     {
  
     }
- 
-    private void LateUpdate()
-    {
-        CurrentPosition = -Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
- 
-        if (Input.GetMouseButton(0))
-        {
-            Vector2 pos = (CurrentPosition - LastPosition);
-            this.transform.position += new Vector3(pos.x, pos.y, 0);
-        }
- 
-        LastPosition = -Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane)); //This line was the problem LastPosition = CurrentPosition may have overwritten some values???
-    }
 
 }
